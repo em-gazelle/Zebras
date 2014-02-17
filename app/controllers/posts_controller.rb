745872738f7bb499357a9452f5501673acbf552c
em-gazelle <emmaf9@gmail.com>
2014-02-17 13:30:14 -0800

    CRUD completed for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,27 @@ class PostsController < ApplicationController
 		@posts = Post.all
 	end
 
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update(params[:post].permit(:title, :text))
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to posts_path
+	end
+
 	private
 		def post_params
 			params.require(:post).permit(:title, :text)
