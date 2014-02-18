0220df69e3d8fad22b0935c4998b74f3326c5449
em-gazelle <emmaf9@gmail.com>
2014-02-17 17:36:53 -0800

    CRD Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,8 +4,18 @@ class CommentsController < ApplicationController
 	end	
 	def create
 		@post = Post.find(params[:post_id])
-		@comment = 
-			@post.comments.create(params[:comment])
-			redirect_to post_path(@post)
+		@comment = @post.comments.create(params[:comment])
+		redirect_to post_path(@post)
+	end
+	def edit
+		@comment = Comment.find(params[:post_id])
+		@comment = @post.comments.update(params[:comment])
+		redirect_to post_path(@post)
+	end
+	def destroy
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.find(params[:id])
+		@comment.destroy
+		redirect_to post_path(@post)
 	end
 end 
